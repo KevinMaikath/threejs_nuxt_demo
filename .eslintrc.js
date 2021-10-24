f46d8b699b3aa9ad8520b7681f2204a32e92079c
Kevin Maikath <kevin.maikath@with-madrid.com>
2021-10-24 11:32:17 +0200

    Added ESLint and Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,22 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended',
-    'prettier'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
-}
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+    },
+    parser: "vue-eslint-parser",
+    extends: [
+        "@nuxtjs/eslint-config-typescript",
+        "eslint:recommended",
+        "prettier",
+        "plugin:prettier/recommended",
+        "plugin:nuxt/recommended",
+    ],
+    plugins: ["prettier"],
+    // add your custom rules here
+    rules: {
+        "no-use-before-define": "off",
+        camelcase: "off",
+        "no-unused-vars": "off",
+    },
+};
