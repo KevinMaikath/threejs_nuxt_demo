323f97a4191496afa78bf98a09742c9c4a3cebe9
Kevin Maikath <kevin.maikath@with-madrid.com>
2022-04-16 10:21:03 +0100

    Enabled SSR and vercel.json for deployments to Vercel with CI

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,6 @@
 export default {
-    // Global page headers: https://go.nuxtjs.dev/config-head
+    ssr: true,
+
     head: {
         title: "threejs_nuxt_demo",
         htmlAttrs: {
@@ -19,21 +20,16 @@ export default {
 
     css: ["@/assets/css/index.scss"],
 
-    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
     plugins: [],
 
-    // Auto import components: https://go.nuxtjs.dev/config-components
     components: true,
 
-    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
     buildModules: [
         // https://go.nuxtjs.dev/typescript
         "@nuxt/typescript-build",
     ],
 
-    // Modules: https://go.nuxtjs.dev/config-modules
     modules: [],
 
-    // Build Configuration: https://go.nuxtjs.dev/config-build
     build: {},
 };
