9a4dacd10a6a8fd021afe8cd286f78935fb39947
Kevin Maikath <kevin.maikath@with-madrid.com>
2023-01-03 18:12:25 +0000

    Lesson 35 (Mixing HTML and WebGL)

--- pages/lessons/35-mixing-html-and-webgl.vue
+++ pages/lessons/35-mixing-html-and-webgl.vue
@@ -2,13 +2,23 @@
     <div>
         <canvas ref="canvas" class="full-screen"></canvas>
 
-        <!-- Loading bar -->
-        <!-- We're doing it with an HTML <div> but we could've done it with a plane with a Shader material. -->
         <div
             id="loading-bar"
             ref="loading_bar"
             :class="{ finished: loaded }"
         ></div>
+
+        <div
+            v-for="(point, index) in infoPoints"
+            :ref="`point_${index}`"
+            :key="index"
+            class="point"
+        >
+            <span class="label">{{ index + 1 }}</span>
+            <span class="text"
+                >Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span
+            >
+        </div>
     </div>
 </template>
 
@@ -25,13 +35,20 @@ import {
     Mesh,
     MeshStandardMaterial,
     PlaneGeometry,
+    Raycaster,
     ShaderMaterial,
+    Vector3,
 } from "three";
 import { gsap } from "gsap";
 import LessonSetupMixin from "~/mixins/lesson-setup.vue";
 
+type InfoPoint = {
+    position: Vector3;
+    element?: HTMLDivElement;
+};
+
 @Component
-export default class IntroAndLoadingLesson extends LessonSetupMixin {
+export default class MixingHtmlAndWebGlLesson extends LessonSetupMixin {
     canvas!: HTMLCanvasElement;
     controls!: OrbitControls;
 
@@ -54,28 +71,29 @@ export default class IntroAndLoadingLesson extends LessonSetupMixin {
 
     ENV_MAP_INTENSITY = 2.5;
 
-    /**
-     * There are multiple ways to add a loading animation:
-     * - Animate the <canvas> in CSS
-     * - Animate a <div> above the <canvas> in CSS
-     * - Animate a black rectangle in front of the camera
-     *
-     * In general, it's recommended to keep everything inside WebGL. So we will go for the 3rd option here.
-     */
+    infoPoints: InfoPoint[] = [
+        { position: new Vector3(1.55, 0.3, -0.6) },
+        { position: new Vector3(0.5, 0.8, -1.6) },
+        { position: new Vector3(1.6, -1.3, -0.7) },
+    ];
+
+    raycaster = new Raycaster();
+
     mounted() {
         this.canvas = this.$refs.canvas as HTMLCanvasElement;
         this.setUp();
+        this.camera.position.set(4, 1, -4);
         this.setUpOrbitControls();
 
         this.configureRenderer();
-
         this.addLoadingOverlay();
-
         this.setUpLoaders();
 
         this.addLights();
         this.loadEnvironmentMap();
         this.loadHelmet();
+
+        this.getInfoPointsRef();
 
         this.setUpAnimation();
         this.startAnimation();
@@ -119,11 +137,10 @@ export default class IntroAndLoadingLesson extends LessonSetupMixin {
 
     async loadHelmet() {
         const gltf = await this.gltfLoader.loadAsync(
-            "/models/FlightHelmet/glTF/FlightHelmet.gltf"
+            "/models/DamagedHelmet/glTF/DamagedHelmet.gltf"
         );
 
-        gltf.scene.scale.set(10, 10, 10);
-        gltf.scene.position.set(0, -4, 0);
+        gltf.scene.scale.set(2.5, 2.5, 2.5);
         gltf.scene.rotation.y = Math.PI * 0.5;
         this.scene.add(gltf.scene);
 
@@ -145,6 +162,8 @@ export default class IntroAndLoadingLesson extends LessonSetupMixin {
     }
 
     // <From Lesson 25/>
+
+    // <From Lesson 34>
 
     setUpLoaders() {
         this.loadingBar = this.$refs.loading_bar as HTMLDivElement;
@@ -219,12 +238,74 @@ export default class IntroAndLoadingLesson extends LessonSetupMixin {
         this.scene.add(this.overlay);
     }
 
+    // <From Lesson 34 />
+
+    /**
+     * Get the reference of all the Info Points from the DOM
+     */
+    getInfoPointsRef() {
+        this.infoPoints.forEach((point, index) => {
+            const ref = `point_${index}`;
+            const refList = this.$refs[ref] as HTMLDivElement[];
+
+            // When the :ref is inside a v-for, it always returns a list of references.
+            // Here we assume that the list will have only one item.
+            if (refList && refList.length > 0) {
+                point.element = refList[0];
+            }
+        });
+    }
+
     setUpAnimation() {
         // const clock = new THREE.Clock();
         // let previousTime = 0;
 
         this.animation = (_: number) => {
             this.controls.update();
+
+            // Don't run any logic for the Info points until the scene is ready
+            if (!this.loaded) return;
+
+            for (const point of this.infoPoints) {
+                if (!point.element) continue;
+
+                const screenPosition = point.position.clone();
+
+                // From the docs: this will project the vector from the world space into the given camera's normalized
+                // device coordinate (NDC) space.
+                screenPosition.project(this.camera);
+
+                /** Test if the point should be visible right now */
+
+                // Here we can use the screenPosition (NDC) instead of the coordinates of the mouse.
+                this.raycaster.setFromCamera(screenPosition, this.camera);
+                const intersects = this.raycaster.intersectObjects(
+                    this.scene.children,
+                    true
+                );
+
+                if (intersects.length === 0) {
+                    point.element.classList.add("visible");
+                } else {
+                    // Show the point if there isn't any other object in front of it
+                    const intersectionDistance = intersects[0].distance;
+                    const pointDistance = point.position.distanceTo(
+                        this.camera.position
+                    );
+
+                    if (pointDistance < intersectionDistance) {
+                        point.element.classList.add("visible");
+                    } else {
+                        point.element.classList.remove("visible");
+                    }
+                }
+
+                // Transform the normalized values to pixels in the current viewport.
+                const translateX = screenPosition.x * this.sizes.width * 0.5;
+                const translateY = -screenPosition.y * this.sizes.height * 0.5;
+
+                point.element.style.transform = `translate(${translateX}px, ${translateY}px)`;
+            }
         };
     }
 }
@@ -253,6 +334,72 @@ $loading-bar-height: 2px;
     &.finished {
         transform-origin: top right; // Make it go to the right
         transition: transform 1s ease-in-out;
+    }
+}
+
+.point {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+
+    .label {
+        position: absolute;
+        top: -20px;
+        left: -20px;
+        width: 40px;
+        height: 40px;
+
+        border-radius: 50%;
+        background-color: #00000077;
+        border: 1px solid #ffffff77;
+        color: #ffffff;
+
+        font-family: Helvetica, Arial, sans-serif;
+        text-align: center;
+        line-height: 40px;
+        font-size: 14px;
+
+        cursor: help;
+
+        // Initial state
+        transform: scale(0, 0);
+        transition: transform 0.3s;
+        will-change: transform;
+    }
+
+    .text {
+        position: absolute;
+        top: 30px;
+        left: -120px;
+        width: 200px;
+        padding: 20px;
+
+        border-radius: 4px;
+        background-color: #00000077;
+        border: 1px solid #ffffff77;
+        color: #ffffff;
+
+        font-family: Helvetica, Arial, sans-serif;
+        text-align: center;
+        line-height: 1.3em;
+        font-size: 14px;
+
+        // Initial state
+        opacity: 0;
+        transition: opacity 0.3s;
+        will-change: opacity;
+
+        // This will prevent the text from showing when hovering the hidden text
+        pointer-events: none;
+    }
+
+    // Show the text when hovering the point
+    &:hover .text {
+        opacity: 1;
+    }
+
+    &.visible .label {
+        transform: scale(1, 1);
     }
 }
 </style>
